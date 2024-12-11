dfc579e2a260d0397ddafe06af7a9a0191fdd288
Spyridon Manolatos <164007448+spyrosManolatos@users.noreply.github.com>
2024-12-11 16:11:59 +0200

    Update question5.sh
    done.
    --> modify aris ilias standards(he asked sth else after all)
    --> modify ages.txt(he asked only the age groups of the passengers)
    remains to merge them

--- exercise1/question5.sh
+++ exercise1/question5.sh
@@ -35,26 +35,33 @@ print_lines_for_age_group() {
         count_19_35 = 0
         count_36_50 = 0
         count_51 = 0
+        print "=== Age Group For Passengers ==="
     }
     NR==1 {
         print $0
     }
     NR>1 {
         if($3 >= 0 && $3 <= 18) {
-            group1[NR] = $0
-            count_0_18++
+            if(toupper($5) ~  /^[[:space:]]*PASSENGER[[:space:]]*$/){
+                group1[count_0_18++] = $0
+            }
+            
         }
         if($3 > 18 && $3 <= 35) {
-            group2[NR] = $0
-            count_19_35++
+            if(toupper($5) ~  /^[[:space:]]*PASSENGER[[:space:]]*$/){
+                group2[count_19_35++] = $0
+            }
+            
         }
         if($3 >= 36 && $3 <=50){
-            group3[NR] = $0
-            count_36_50++      
+            if(toupper($5) ~  /^[[:space:]]*PASSENGER[[:space:]]*$/){
+                group3[count_36_50++] = $0
+            }   
         }
         if($3 >=51){
-            group4[NR] = $0
-            count_51++
+            if(toupper($5) ~  /^[[:space:]]*PASSENGER[[:space:]]*$/){
+                group4[count_51++] = $0
+            }
         }
     }
     END {
@@ -93,73 +100,117 @@ process_age_group_csvs(){
     {
         awk -F, '
         BEGIN {
-            percentage_for_0_18 = 0
-            rescued_0_18 = 0
-            count_0_18 = 0
-            rescued_19_35 = 0
-            rescued_36_50 = 0
-            rescued_51 = 0
+            percentage_for_0_18_pass = 0
+            percentage_for_19_35_pass = 0
+            percentage_for_36_50_pass = 0
+            percentage_for_51_pass = 0
+
+            percentage_for_0_18_crew = 0
+            percentage_for_19_35_crew = 0
+            percentage_for_36_50_crew = 0
+            percentage_for_51_crew = 0
+            
+            count_0_18_for_passengers = 0
+            count_19_35_for_passengers = 0
+            count_36_50_for_passengers = 0
+            count_51_for_passengers = 0
+            count_0_18_for_crew = 0
+            count_19_35_for_crew = 0
+            count_36_50_for_crew = 0
+            count_51_for_crew = 0
+
+            rescued_0_18_for_passengers = 0
+            rescued_19_35_for_passengers = 0
+            rescued_36_50_for_passengers = 0
+            rescued_51_for_passengers = 0
+
+            rescued_0_18_for_crew = 0
+            rescued_19_35_for_crew = 0
+            rescued_36_50_for_crew = 0
+            rescued_51_for_crew = 0
         }
         NR>1 {
             if ($3 >= 0 && $3 <= 18) {
-                count_0_18++
-                if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
-                    rescued_0_18++
+                
+                if(toupper($5) ~  /^[[:space:]]*PASSENGER[[:space:]]*$/){
+                    count_0_18_for_passengers++
+                    if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
+                        rescued_0_18_for_passengers++
+                    }
+                }
+                else{
+                    count_0_18_for_crew++
+                    if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
+                        rescued_0_18_for_crew++
+                    }
                 }
             }
             else if ($3 > 18 && $3 <= 35) {
-                count_19_35++
-                if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
-                    rescued_19_35++
+                if(toupper($5) ~  /^[[:space:]]*PASSENGER[[:space:]]*$/){
+                    count_19_35_for_passengers++
+                    if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
+                        rescued_19_35_for_passengers++
+                    }
+                }
+                else{
+                    count_19_35_for_crew++
+                    if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
+                        rescued_19_35_for_crew++
+                    }
                 }
             }
             else if ($3 >= 36 && $3 <= 50) {
-                count_36_50++
-                if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
-                    rescued_36_50++
+                if(toupper($5) ~  /^[[:space:]]*PASSENGER[[:space:]]*$/){
+                    count_36_50_for_passengers++
+                    if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
+                        rescued_36_50_for_passengers++
+                    }
+                }
+                else{
+                    count_36_50_for_crew++
+                    if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
+                        rescued_36_50_for_crew++
+                    }
                 }
             }
             else if ($3 >= 51) {
-                count_51++
-                if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
-                    rescued_51++
+                if(toupper($5) ~  /^[[:space:]]*PASSENGER[[:space:]]*$/){
+                    count_51_for_passengers++
+                    if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
+                        rescued_51_for_passengers++
+                    }
+                }
+                else{
+                    count_51_for_crew++
+                    if (toupper($6) ~ /^[[:space:]]*YES[[:space:]]*$/) {
+                        rescued_51_for_crew++
+                    }
                 }
             }
             
         }
         END {
-            print "\nAge Group 0-18 Summary:"
-            print "------------------------"
-            print "Total passengers:", count_0_18
-            print "Rescued passengers:", rescued_0_18
-            if (count_0_18 > 0) {
-                percentage_for_0_18 = (rescued_0_18/count_0_18) * 100
-                printf "Rescue percentage: %.2f%%\n", percentage_for_0_18
-            }
-            print "\nAge Group 19-35 Summary:"
-            print "------------------------"
-            print "Total passengers:", count_19_35
-            print "Rescued passengers:", rescued_19_35
-            if (count_19_35 > 0) {
-                percentage_for_19_35 = (rescued_19_35/count_19_35) * 100
-                printf "Rescue percentage: %.2f%%\n", percentage_for_19_35
-            }
-            print "\nAge Group 36-50 Summary:"
-            print "------------------------"
-            print "Total passengers:", count_36_50
-            print "Rescued passengers:", rescued_36_50
-            if (count_36_50 > 0) {
-                percentage_for_36_50 = (rescued_36_50/count_36_50) * 100
-                printf "Rescue percentage: %.2f%%\n", percentage_for_36_50
-            }
-            print "\nAge Group >=51 Summary:"
-            print "------------------------"
-            print "Total passengers:", count_51
-            print "Rescued passengers:", rescued_51
-            if (count_51 > 0) {
-                percentage_for_51 = (rescued_51/count_51) * 100
-                printf "Rescue percentage: %.2f%%\n", percentage_for_51
-            }
+            percentage_for_0_18_crew = (rescued_0_18_for_crew / count_0_18_for_crew) * 100
+            percentage_for_19_35_crew = (rescued_19_35_for_crew / count_19_35_for_crew) * 100
+            percentage_for_36_50_crew = (rescued_36_50_for_crew / count_36_50_for_crew) * 100
+            percentage_for_51_crew = (rescued_51_for_crew / count_51_for_crew) * 100
+            percentage_for_0_18_pass = (rescued_0_18_for_passengers / count_0_18_for_passengers) * 100
+            percentage_for_19_35_pass = (rescued_19_35_for_passengers / count_19_35_for_passengers) * 100
+            percentage_for_36_50_pass = (rescued_36_50_for_passengers / count_36_50_for_passengers) * 100
+            percentage_for_51_pass = (rescued_51_for_passengers / count_51_for_passengers) * 100
+            print "Rescued Report"
+            print "Age Group 0-18 for passengers:", rescued_0_18_for_passengers, "out of", count_0_18_for_passengers, "(", percentage_for_0_18_pass, "%)"
+            print "Age Group 19-35 for passengers:", rescued_19_35_for_passengers, "out of", count_19_35_for_passengers, "(", percentage_for_19_35_pass, "%)"
+            print "Age Group 36-50 for passengers:", rescued_36_50_for_passengers, "out of", count_36_50_for_passengers, "(", percentage_for_36_50_pass, "%)"
+            print "Age Group 51+ for passengers:", rescued_51_for_passengers, "out of", count_51_for_passengers, "(", percentage_for_51_pass, "%)"
+            print "Age Group 0-18 for crew:", rescued_0_18_for_crew, "out of", count_0_18_for_crew, "(", percentage_for_0_18_crew, "%)"
+            print "Age Group 19-35 for crew:", rescued_19_35_for_crew, "out of", count_19_35_for_crew, "(", percentage_for_19_35_crew, "%)"
+            print "Age Group 36-50 for crew:", rescued_36_50_for_crew, "out of", count_36_50_for_crew, "(", percentage_for_36_50_crew, "%)"
+            print "Age Group 51+ for crew:", rescued_51_for_crew, "out of", count_51_for_crew, "(", percentage_for_51_crew, "%)"
+            print "Total rescued passengers:", rescued_0_18_for_passengers + rescued_19_35_for_passengers + rescued_36_50_for_passengers + rescued_51_for_passengers
+            print "Total rescued crew:", rescued_0_18_for_crew + rescued_19_35_for_crew + rescued_36_50_for_crew + rescued_51_for_crew
+            print "Total rescued:", rescued_0_18_for_passengers + rescued_19_35_for_passengers + rescued_36_50_for_passengers + rescued_51_for_passengers + rescued_0_18_for_crew + rescued_19_35_for_crew + rescued_36_50_for_crew + rescued_51_for_crew
+
         }' "$working_file"
     } > percentages.txt
 }
@@ -172,14 +223,23 @@ calc_avg_age_per_passnger_catagory(){
         BEGIN {
             total_age = 0
             total_passengers = 0
-            total_children = 0
-            total_adults_for_19_35 = 0
-            total_adults_for_35_50 = 0
+            total_children_crew = 0
+            total_adults_for_19_35_crew = 0
+            total_adults_for_35_50_crew = 0
+            total_seniors_crew = 0
+            total_children_passengers = 0
+            total_adults_for_19_35_passengers = 0
+            total_adults_for_35_50_passengers = 0
+            total_seniors_passengers = 0
             total_seniors = 0
-            total_age_for_children = 0
-            total_age_for_adults_for_19_35 = 0
-            total_age_for_adults_for_35_50 = 0
-            total_age_for_seniors = 0
+            total_age_for_children_pass = 0
+            total_age_for_adults_for_19_35_pass = 0
+            total_age_for_adults_for_35_50_pass = 0
+            total_age_for_seniors_pass = 0
+            total_age_for_children_crew = 0
+            total_age_for_adults_for_19_35_crew = 0
+            total_age_for_adults_for_35_50_crew = 0
+            total_age_for_seniors_crew = 0
         }
         NR>1 {
             total_age += $3
@@ -188,30 +248,54 @@ calc_avg_age_per_passnger_catagory(){
             if($5 ~ /^[[:space:]]*Passenger[[:space:]]*$/) {
                 total_rescued++
                 if ($3 >= 0 && $3 <= 18) {
-                    total_children++
-                    total_age_for_children += $3
+                    total_children_passengers++
+                    total_age_for_children_pass += $3
                 }
                 if ($3 > 19 && $3 <= 35) {
-                    total_adults_for_19_35++
-                    total_age_for_adults_for_19_35 += $3
+                    total_adults_for_19_35_passengers++
+                    total_age_for_adults_for_19_35_pass += $3
                 }
                 else if ($3 > 35 && $3 <= 50) {
-                    total_adults_for_35_50++
-                    total_age_for_adults_for_35_50 += $3
+                    total_adults_for_35_50_passengers ++
+                    total_age_for_adults_for_35_50_pass += $3
                 }
                 else if ($3 > 50) {
-                    total_seniors++
-                    total_age_for_seniors += $3
+                    total_seniors_passengers++
+                    total_age_for_seniors_pass += $3
+                }
+            }
+            else {
+                total_rescued++
+                if ($3 >= 0 && $3 <= 18) {
+                    total_children_crew++
+                    total_age_for_children_crew += $3
+                }
+                if ($3 > 19 && $3 <= 35) {
+                    total_adults_for_19_35_crew++
+                    total_age_for_adults_for_19_35_crew += $3
+                }
+                else if ($3 > 35 && $3 <= 50) {
+                    total_adults_for_35_50_crew++
+                    total_age_for_adults_for_35_50_crew += $3
+                }
+                else if ($3 > 50) {
+                    total_seniors_crew++
+                    total_age_for_seniors_crew += $3
                 }
             }
             
         }
         END {
-            print "Average age of all passengers:", total_age/total_passengers
-            print "Average age of children (0-18):", total_age_for_children/total_children
-            print "Average age of adults (19-35):", total_age_for_adults_for_19_35/total_adults_for_19_35
-            print "Average age of adults (36-50):", total_age_for_adults_for_35_50/total_adults_for_35_50
-            print "Average age of seniors (51+):", total_age_for_seniors/total_seniors
+            print "Average age for passengers"
+            print "Children (0-18):", total_age_for_children_pass / total_children_passengers
+            print "Adults (19-35):", total_age_for_adults_for_19_35_pass / total_adults_for_19_35_passengers
+            print "Adults (36-50):", total_age_for_adults_for_35_50_pass / total_adults_for_35_50_passengers
+            print "Seniors (51+):", total_age_for_seniors_pass / total_seniors_passengers
+            print "Average age for crew"
+            print "Children (0-18):", total_age_for_children_crew / total_children_crew
+            print "Adults (19-35):", total_age_for_adults_for_19_35_crew / total_adults_for_19_35_crew
+            print "Adults (36-50):", total_age_for_adults_for_35_50_crew / total_adults_for_35_50_crew
+            print "Seniors (51+):", total_age_for_seniors_crew / total_seniors_crew
         }' "$working_file"
     } > avg.txt
 }
