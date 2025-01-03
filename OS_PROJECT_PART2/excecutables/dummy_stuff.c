5c542e04cf0246f25625cbe71e18d9d2a8726608
Spyridon Manolatos <164007448+spyrosManolatos@users.noreply.github.com>
2025-01-03 15:47:06 +0200

    Create dummy_stuff.c

---
+++ OS_PROJECT_PART2/excecutables/dummy_stuff.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+double a = 1.1;
+
+void core_delay()
+{
+	unsigned long j;
+
+	for (j = 0; j < 100000; j++) {
+		a += sqrt(1.1)*sqrt(1.2)*sqrt(1.3)*sqrt(1.4)*sqrt(1.5);
+		a += sqrt(1.6)*sqrt(1.7)*sqrt(1.8)*sqrt(1.9)*sqrt(2.0);
+		a += sqrt(1.1)*sqrt(1.2)*sqrt(1.3)*sqrt(1.4)*sqrt(1.5);
+		a += sqrt(1.6)*sqrt(1.7)*sqrt(1.8)*sqrt(1.9);
+	}
+}
+
+
+
+int main(int argc, char **argv)
+{
+	int pid = getpid();
+
+	printf("process %d begins\n", pid);
+	core_delay();
+	printf("process %d ends\n", pid);
+
+	return 0;
+}
