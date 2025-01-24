88320752c66bcbb6a3272df6d7c3dbc575fa0d0b
spyrosManolatos <spmanolatos@gmail.com>
2025-01-24 18:43:27 +0200

    final final version in comments

--- OS_PROJECT_PART2/IMPLEMENTATION/scheduler/run.sh
+++ OS_PROJECT_PART2/IMPLEMENTATION/scheduler/run.sh
@@ -1,4 +1,4 @@
 ./exercise1 4 FCFS order_of_exec.txt > fcfs_exercise1.txt # exercise 1 FCFS with 4 cores and exercise 1
 ./exercise1 4 RR 3 order_of_exec.txt > rr_exercise1.txt # exercise 1 RR with 4 cores and quantum=3
-./exercise2 2 FCFS order_of_exec.txt > fcfs_exercise2.txt # exercise 2 FCFS with 2 cores and each core assigned
+./exercise2 2 FCFS order_of_exec.txt > fcfs_exercise2.txt # exercise 2 FCFS with 2 cores and one process multiple cores
 ./exercise1 4 RRAFF 3 order_of_exec.txt > rrff_exercise1.txt # exercise 1 RRAFF with 4 cores and quantum=3
